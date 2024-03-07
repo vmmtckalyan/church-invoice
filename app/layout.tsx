d0348733237777c55dd59bc8e6277fba571411fa
Satish Puli <satish.puli@mintifi.com>
2024-03-07 07:24:57 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,10 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: 'VMMTC Dashboard',
+    default: 'VMMTC Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  description: 'VMMTC Dashboard',
 };
 export default function RootLayout({
   children,
